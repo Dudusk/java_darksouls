1abc72c2cdc8da10aa8e74e37164a92f6960ba5e
Dusk <tanguy.pave@ynov.com>
2018-04-29 22:43:52 +0200

    Bag

--- src/lsg/bags/Bag.java
+++ src/lsg/bags/Bag.java
@@ -1,8 +1,11 @@
 package lsg.bags;
 
 import java.util.HashSet;
+import java.util.Iterator;
 
-public class Bag {
+import lsg.consumables.Consumable;
+
+public class Bag extends HashSet<Collectible>{
 
     private int capacity;
     private int weight;
@@ -22,25 +25,82 @@ public class Bag {
         return weight;
     }
 
-
+    /**
+     * Ajoute un item dans le sac
+     * @param item
+     */
     public void push(Collectible item) {
+    	
+    	weight = getTotalWeight();
+    	
         if((capacity - weight) == 0) {
             System.out.println("Your bag is full");
+        } else if (weight + item.getWeight() <= capacity) {
+        	weight += item.getWeight();
+        	items.add(item);
         }
     }
 
-
-    public Collectible pop(Collectible item){
-
+    
+    public Collectible pop (Collectible item) {
+    	
+    	
+    	
+    	return item;
     }
 
 
-    public boolean contains(Collectible item) {
-
+    public boolean contains (Collectible item) {
+    	Collectible tableau[] = getItems();
+    	
+    	for (int i = 0; i<tableau.length; i++) {
+    		if(item.getClass().getSimpleName() == tableau[i].getClass().getSimpleName()) {
+    			return true;
+    		} else {
+    			return false;
+    		}
+    	}
+    	
+    	return false;
     }
+    
 
     public Collectible[] getItems(){
-
+    	Collectible itemsOfBag[] = {};
+    	
+    	Collectible ligne;
+        int i = 0;
+        Iterator<Collectible> itr = iterator();
+        
+        while(itr.hasNext()){
+        	i++;
+        	ligne = itr.next();
+        	itemsOfBag[i] = ligne;
+        	System.out.println(ligne);  
+        }
+    	
+    	return itemsOfBag;
+    }
+    
+    /**
+     * Calcul la totalité du poids du sac
+     * @return total
+     */
+    private int getTotalWeight() {
+    	int total = 0;
+    	
+    	Collectible ligne;
+        int i = 0;
+        Iterator<Collectible> itr = iterator();
+        
+        while(itr.hasNext()){
+        	i++;
+        	ligne = itr.next();
+        	total = ligne.getWeight();
+        	System.out.println(ligne.getWeight());  
+        }
+    	
+    	return total;
     }
 
 
